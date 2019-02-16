3b91af21d824ffd1bcb31f2b4e766e2e95663e4d
Jayendra Minakshisundar <jayendraminakshisundar@gmail.com>
2019-02-16 14:46:55 -0600

    building list of dicts from AnnotateImageResponses

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -3,13 +3,16 @@ from google.cloud import vision
 client = vision.ImageAnnotatorClient.from_service_account_file('/Users/jayu/Documents/dev/uncommonhacks2019/credentials/uncommonhacks-memesearch-7a7faabf16ff.json')
 
 def process_image_urls(urls):
-    requests = []
-    for url in urls:
-        requests.append({'image': {'source': {'image_uri': url}},
+    results = []
+    for counter, url in enumerate(urls):
+        request = {'image': {'source': {'image_uri': url}},
                         'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
-                        'image_context': {"language_hints": ['en']}})
-    responses = client.batch_annotate_images(requests)
-    print(responses)
+                        'image_context': {"language_hints": ['en']}}
+        response = client.annotate_image(request)
+        results.append({'id': counter,
+                        'url': url,
+                        'text': response.full_text_annotation.text})
+    return results
 
 process_image_urls(['https://afinde-production.s3.amazonaws.com/uploads/card_1dd616ad-f0d0-45d3-8909-24b578014654.jpg', 'https://i.redd.it/h4rntqluid911.jpg'])
 
