aa65470be00480a0d33a810ae64fbc9597725964
Mirko Mantovani <mirko.mantovani@mail.polimi.it>
2019-02-16 14:47:10 -0600

    Got list of URLs memes from reddit

--- reddit_memes_downloader.py
+++ reddit_memes_downloader.py
@@ -14,8 +14,8 @@ def getPosts(subreddit, postLimit):
     r = requests.get(url, headers=headers)
     if r.status_code == requests.codes.ok:
         data = r.json()
-        print('Sleeping for 3 seconds...\n')
-        time.sleep(3)
+        print('Sleeping for 1 seconds...\n')
+        time.sleep(1)
         return data['data']['children']
     else:
         print('Sorry, but there was an error retrieving the subreddit\'s data!')
@@ -33,12 +33,30 @@ def saveImages(posts, scoreLimit, save_dir='reddit_memes'):
             saveImage(url, title, save_dir)
 
 
+def extractURLs(posts, scoreLimit, save_dir='reddit_memes'):
+    print('extract URLS')
+    images_urls = []
+    for post in posts:
+        url = post['data']['url']
+        print(url)
+        score = post['data']['score']
+        title = post['data']['title']
+        allowed_suffixes = (".png", ".jpg", ".gif")
+        if str(url).endswith(allowed_suffixes):
+            images_urls.append(str(url))
+        else:
+            print('not in allowed suffixes')
+
+
+    return images_urls
+
+
 def saveImage(url, title, save_dir='reddit_memes'):
     global counter
     save_dir = makeSaveDir(save_dir)
     dot_location = url.rfind('.')
-    print(title)
-    print(url)
+    # print(title)
+    # print(url)
     filename = './' + save_dir+title
     # filename = (save_dir + title.replace('/', ':') + url[dot_location: dot_location + 4]).encode('utf-8')
     if not os.path.exists(filename):
@@ -56,8 +74,11 @@ def makeSaveDir(dir):
 def downloadImagesFromReddit(subreddits = 'AdviceAnimals', postLimit=10, scoreLimit=20):
     for subreddit in subreddits:
         posts = getPosts(subreddit, postLimit)
-        saveImages(posts, scoreLimit, subreddit.lower())
-    print(str(counter) + ' images have been scraped!')
+        # we don't need to store images, just need the URLs to make a request to google vision API
+        memes_urls = extractURLs(posts, scoreLimit, subreddit.lower())
+        # saveImages(posts, scoreLimit, subreddit.lower())
+    print(str(len(memes_urls)) + ' images have been scraped!')
+    print(memes_urls)
 
 
 def main():
