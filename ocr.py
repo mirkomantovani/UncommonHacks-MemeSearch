dc02bbc8e6688c1010ea95801783b44aba2d6e8f
Jayendra Minakshisundar <jayendraminakshisundar@gmail.com>
2019-02-16 19:53:52 -0600

    request 16 images at a time

--- ocr.py
+++ ocr.py
@@ -3,17 +3,24 @@ import pickle
 
 image_client = vision.ImageAnnotatorClient.from_service_account_file('uncommonhacks-memesearch-7a7faabf16ff.json')
 
+def chunks(urls):
+    for i in range(0, len(urls), 16):
+        yield urls[i:i+16]
+
 def process_image_urls(urls):
     requests = []
     results = {}
-    for url in urls:
-        request = {'image': {'source': {'image_uri': url}},
-                        'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
-                        'image_context': {"language_hints": ['en']}}
-        requests.append(request)
-    response = image_client.batch_annotate_images(requests)
-    for counter, url in enumerate(urls):
-        results[url] = response.responses[counter].full_text_annotation.text
+    chunked_urls = chunks(urls)
+    for chunk in chunked_urls:
+        for url in chunk:
+            request = {'image': {'source': {'image_uri': url}},
+                       'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
+                       'image_context': {"language_hints": ['en']}}
+            requests.append(request)
+        response = image_client.batch_annotate_images(requests)
+        for counter, url in enumerate(chunk):
+            results[url] = response.responses[counter].full_text_annotation.text
+        requests.clear()
     return results
 
 #urls = [
