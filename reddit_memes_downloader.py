079b00e8e23e10e63392c0170923a832842c7995
Jayendra Minakshisundar <jayendraminakshisundar@gmail.com>
2019-02-17 01:47:04 -0600

    using pushshift.io and built database

--- reddit_memes_downloader.py
+++ reddit_memes_downloader.py
@@ -5,93 +5,50 @@ from inverted_index_builder import build_inverted_index
 import pickle
 import ocr
 
-counter = 0
-
-
-def getPosts(subreddit, postLimit):
+def getPosts(subreddit, n):
     # print('getPosts')
-    url = 'http://www.reddit.com/r/' + subreddit + '/.json?limit=' + str(postLimit)
-    headers = {
-    'User-Agent': 'Reddit Wallpaper Scraper 1.0'
-    }
-    r = requests.get(url, headers=headers)
-    if r.status_code == requests.codes.ok:
-        data = r.json()
-        print('Sleeping for 1 seconds...\n')
-        time.sleep(1)
-        return data['data']['children']
-    else:
-        print('Sorry, but there was an error retrieving the subreddit\'s data!')
-        return None
+    urls = set()
+    seconds = int(time.time())
+    counter = 0
+    for epoch_time in range(seconds, seconds - 21600 * n, -21600):
+        print(counter)
+        counter += 1
+        url = f'https://api.pushshift.io/reddit/submission/search/?sort=new&subreddit={subreddit}&before={epoch_time}'
+        headers = {
+            'User-Agent': 'Reddit Wallpaper Scraper 1.0'
+        }
+        r = requests.get(url, headers=headers)
+        if r.status_code == requests.codes.ok:
+            data = r.json()
+            for post in data['data']:
+                urls.add(post['url'])
+    return urls
 
-
-def saveImages(posts, scoreLimit, save_dir='reddit_memes'):
-    # print('saveImages')
-    for post in posts:
-        url = post['data']['url']
-        # print(url)
-        score = post['data']['score']
-        title = post['data']['title']
-        if 'i.imgur.com' in url and score > scoreLimit:
-            saveImage(url, title, save_dir)
-
-
-def extractURLs(posts, scoreLimit, save_dir='reddit_memes'):
-    # print('extract URLS')
+def extractURLs(urls):
     images_urls = []
-    for post in posts:
-        url = post['data']['url']
+    allowed_suffixes = (".png", ".jpg", ".gif")
+    for url in urls:
         # print(url)
-        score = post['data']['score']
-        title = post['data']['title']
-        allowed_suffixes = (".png", ".jpg", ".gif")
-        if str(url).endswith(allowed_suffixes):
-            images_urls.append(str(url))
+        if url.endswith(allowed_suffixes):
+            images_urls.append(url)
         else:
             print('not in allowed suffixes')
-
-
     return images_urls
-
-
-def saveImage(url, title, save_dir='reddit_memes'):
-    global counter
-    save_dir = makeSaveDir(save_dir)
-    dot_location = url.rfind('.')
-    # print(title)
-    # print(url)
-    filename = './' + save_dir+title
-    # filename = (save_dir + title.replace('/', ':') + url[dot_location: dot_location + 4]).encode('utf-8')
-    if not os.path.exists(filename):
-        # print('Saving ' + filename + '!\n')
-        counter += 1
-        urllib.request.urlretrieve(url, filename)
-
-
-def makeSaveDir(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    return dir + '/'
-
 
 # Calls Reddit API, retrieves JSON file, parses it and gets all urls in the specified
 # subreddit with a limit of postLimit
-def downloadImagesFromReddit(subreddits = 'AdviceAnimals', postLimit=100, scoreLimit=20):
-    for subreddit in subreddits:
-        posts = getPosts(subreddit, postLimit)
-        # we don't need to store images, just need the URLs to make a request to google vision API
-        memes_urls = extractURLs(posts, scoreLimit, subreddit.lower())
-        # saveImages(posts, scoreLimit, subreddit.lower())
-    print(str(len(memes_urls)) + ' images have been scraped!')
-    # print(memes_urls)
-    return memes_urls
+def get_img_urls_from_subreddit(subreddit = 'AdviceAnimals'):
+    urls = getPosts(subreddit, 1000)
+    img_urls = extractURLs(urls)
+    print(len(img_urls))
+    return img_urls
 
 
 def main():
     if len(sys.argv) > 1:
-        meme_urls = downloadImagesFromReddit(sys.argv[1:])
+        meme_urls = get_img_urls_from_subreddit(sys.argv[1])
     else:
-        meme_urls = downloadImagesFromReddit(['AdviceAnimals','memes'])
+        meme_urls = get_img_urls_from_subreddit('AdviceAnimals')
 
 
     # API calls to Google Vision API of GCP
@@ -103,8 +60,8 @@ def main():
     # print(inverted_index)
 
     # Storing inverted index with pickle
-    # with open('memes_inverted_index.pickle', 'wb') as handle:
-    #     pickle.dump(inverted_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('memes_inverted_index.pickle', 'wb') as handle:
+        pickle.dump(inverted_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
